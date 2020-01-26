87c673469fc9cbaa238a13d3c92ad0b83d8acbee
Marko Sterbentz <stermark@isu.edu>
2020-01-26 14:40:40 -0600

    Implemented functions for filtering tweets by subject, looking up movies in IMDb, and finding nominees from tweets.

--- main.py
+++ main.py
@@ -31,13 +31,17 @@ def find_tweets_about_host(data, word_list = []):
 
 def find_tweets_about(tweet_list, subject):
     '''
-    Returns new list of relevant tweets containing subject.
-    :param tweet_list:
-    :param subject:
-    :return:
+    Searches each tweet in the list for the subject.
+    :param tweet_list: The list of tweets to search through.
+    :param subject: String denoting the subject to look for.
+    :return: Returns new list of relevant tweets containing the subject.
     Written by Marko.
     '''
-    print("Not implemented yet")
+    candidate_tweets = []
+    for tweet in tweet_list:
+        if subject in tweet:
+            candidate_tweets.append(tweet)
+    return candidate_tweets
 
 def create_noun_chunks(tweet_list, award_name):
     '''
@@ -91,12 +95,33 @@ def pre_process_data(json_data):
 
 def get_nominees(pre_processed_tweet_list):
     '''
-
-    :param pre_processed_tweet_list:
+    Determines the nominees for each award based on the given list of tweets.
+    :param pre_processed_tweet_list: A list of tweets that have been pre-filtered.
     :return: Dictionary containing 27 keys, with list as its value
     Written by Marko.
     '''
-    print("Not implemented yet")
+
+    num_possible_nominees = 5
+    award_nominees = {}
+
+    # For each award category
+    for category in award_names:
+        # Filter tweets by subject string
+        nominee_tweets = find_tweets_about(pre_processed_tweet_list, 'nomin')
+
+        # Get the nouns chunks in the remaining tweets
+        df_noun_chunks = create_noun_chunks(nominee_tweets, category)
+
+        # Aggregate and sort the noun chunks
+        df_sorted_nouns = aggregate_and_sort_df(df_noun_chunks)
+
+        # Produce the correct number of noun chunks that also exist on IMDb
+        candidate_list = filter_with_imdb(df_sorted_nouns, num_possible_nominees)
+
+        # Perform statistical truncation to get plausible results amd add to map for the current award
+        award_nominees[category] = statistical_truncation(candidate_list, 0.1, num_possible_nominees)
+
+    return award_nominees
 
 def fuzzy_match(s1, s2, threshold):
     '''
@@ -111,21 +136,34 @@ def fuzzy_match(s1, s2, threshold):
 
 def find_imdb_movie(df_row):
     '''
-
-    :param df_row:
-    :return:
+    Searches IMDb to see if the movie title in df_row exists.
+    :param df_row: The movie for which to search IMDb.
+    :return: The proper name of the movie.
     Written by Marko.
     '''
-    print("Not implemented yet")
+    imdb_obj = imdb.IMDb()
+    noun = df_row['word']
+    result = imdb_obj.search_movie(noun)
+    if len(result) == 0 or result[0]['title'] != noun:
+        return None
+    else:
+        return noun
 
 award_names = []
 
 def main():
     data = [json.loads(line) for line in open('gg2020.json','r',encoding='utf-8')]
-    word_list = find_tweets_about_host(data)
-    word_df = pd.DataFrame(word_list, columns=['word'])
-    host = find_imdb_person(aggregate_and_sort_df(word_df))
-    print(host)
+    # word_list = find_tweets_about_host(data)
+    # word_df = pd.DataFrame(word_list, columns=['word'])
+    # host = find_imdb_person(aggregate_and_sort_df(word_df))
+    # print(host)
+    tweet_list = []
+    for d in data:
+        tweet_list.append(d['text'])
+    candidate_tweets = find_tweets_about(tweet_list, 'nomin')
+    for i in range(0, 20):
+        print(candidate_tweets[i])
+        print()
 
 
 t = time.time()

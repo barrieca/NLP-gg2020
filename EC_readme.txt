43f3f41b3f85fcae3376e75b3fae7c1addb4da85
barrieca <cambarrie@comcast.net>
2020-02-06 17:47:22 -0600

    Added EC_readme.txt and year argument for gg_api.py

---
+++ EC_readme.txt
@@ -0,0 +1,16 @@
+Readme information for extra credit
+
+Sentiment Analysis:
+################################################################################
+The sentiment_analysis_helper function is called from the main function of
+gg_api.py. The return value of this function is then printed. As it is
+implemented, the results of this feature are meant to convey the overall
+reception of tweeters to the award winners. The results are shown in the format
+(<winner name>: <sentiment polarity> -> <actual text interpretation of polarity>).
+
+It is expected that the main of gg_api.py will be called after the minimum
+requirement functions are called. Specifically, this feature requires a csv file
+that gets output by the get_winner function. If this csv file is not found, it
+will be recreated (which will be time consuming). When running gg_apy.py a year
+argument is required (i.e. $ python gg_apy.py <year>).
+
--- gg_api.py
+++ gg_api.py
@@ -1,6 +1,7 @@
 '''Version 0.35'''
 
 import gg_func as gg
+import sys
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
@@ -76,14 +77,13 @@ def pre_ceremony():
     print("Pre-ceremony processing complete.")
     return
 
-def main():
+def main(year):
     '''This function calls your program. Typing "python gg_api.py"
     will run this function. Or, in the interpreter, import gg_api
     and then run gg_api.main(). This is the second thing the TA will
     run when grading. Do NOT change the name of this function or
     what it returns.'''
 
-    year = 2013
     # Sentiment Analysis
     data_file_path = 'gg' + str(year) + '.json'
     award_names = OFFICIAL_AWARDS_1315 if int(year) < 2016 else OFFICIAL_AWARDS_1819
@@ -95,4 +95,10 @@ def main():
     return
 
 if __name__ == '__main__':
-    main()
+
+    if len(sys.argv) > 1:
+        year = sys.argv[1]
+        main(year)
+    else:
+        print("Usage:\n$ python gg_api.py <year>")
+
